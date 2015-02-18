freelancer/phlab

commit c6f61940f7a8403e191b66956be8de1bcd6dadf7

Add a HipChat client
Summary: Fixes T19276. Currently, we use [[https://github.com/hipchat/hipchat-php | hipchat/hipchat-php]] as a HipChat client. This is largely overkill because the HipChat API is fairly simple and we can reasonably write a native client instead (thus removing an external dependency). The `HipChatClient` class is basically a port of https://github.com/hipchat/hipchat-php/blob/master/src/HipChat/HipChat.php to use native `libphutil` functionality. In the process, I remove a bunch of user-management functionality which we are unlikely to need.

Test Plan: Created an API key and made a few calls using the new HipChat client.

Reviewers: raymond.huang

Reviewed By: raymond.huang

Maniphest Tasks: T19276

Differential Revision: https://phabricator.freelancer.com/D16981

src/applications/herald/extension/HeraldHipChatNotificationCustomAction.php
@@ -2,10 +2,6 @@
 
 /**
  * A Herald action for sending notifications to HipChat rooms.
- *
- * This class uses [[https://github.com/hipchat/hipchat-php/ |
- * hipchat/hipchat-php]] to communicate with the
- * [[https://www.hipchat.com/docs/api | HipChat API]].
  */
 final class HeraldHipChatNotificationCustomAction extends HeraldCustomAction {
 
@@ -84,27 +80,15 @@ final class HeraldHipChatNotificationCustomAction extends HeraldCustomAction {
 
   /**
    * Create a new HipChat API object.
-   *
-   * @phutil-external-symbol class HipChat\HipChat
    */
   protected function getClient() {
-    Composer::registerAutoloader();
-    return new HipChat\HipChat($this->getApiToken());
-  }
-
-  /**
-   * Retrieve the configured API token.
-   *
-   * @return string  HipChat API token.
-   */
-  private function getApiToken() {
     $token = PhabricatorEnv::getEnvConfig('hipchat.token');
 
     if (!$token) {
       throw new Exception('No HipChat API token specified!');
     }
 
-    return $token;
+    return new HipChatClient($token);
   }
 
   /**
@@ -113,7 +97,6 @@ final class HeraldHipChatNotificationCustomAction extends HeraldCustomAction {
    * @param  string
    * @param  string
    * @param  PhabricatorObjectHandle
-   * @param  map<string, string>
    * @return string
    */
   private function getMessage(

src/__phutil_library_map__.php
@@ -11,6 +11,7 @@ phutil_register_library_map(array(
   'class' => array(
     'Composer' => 'infrastructure/Composer.php',
     'HeraldHipChatNotificationCustomAction' => 'applications/herald/extension/HeraldHipChatNotificationCustomAction.php',
+    'HipChatClient' => 'infrastructure/HipChatClient.php',
     'HipChatConfigOptions' => 'applications/config/option/HipChatConfigOptions.php',
     'PhlabAWSFileStorageEngine' => 'applications/files/engine/PhlabAWSFileStorageEngine.php',
     'PhlabConfigOptions' => 'applications/config/option/PhlabConfigOptions.php',

src/infrastructure/HipChatClient.php
@@ -1,0 +1,222 @@
+<?php
+
+/**
+ * Library for interacting with the HipChat REST API.
+ *
+ * Based on [[https://github.com/hipchat/hipchat-php/ | hipchat/hipchat-php]].
+ *
+ * @see http://api.hipchat.com/docs/api
+ */
+final class HipChatClient {
+
+  const DEFAULT_TARGET = 'https://api.hipchat.com/v1/';
+
+  // Colors.
+  const COLOR_YELLOW = 'yellow';
+  const COLOR_RED = 'red';
+  const COLOR_GRAY = 'gray';
+  const COLOR_GREEN = 'green';
+  const COLOR_PURPLE = 'purple';
+  const COLOR_RANDOM = 'random';
+
+  // Formats.
+  const FORMAT_HTML = 'html';
+  const FORMAT_TEXT = 'text';
+
+  private $apiTarget;
+  private $authToken;
+
+  /**
+   * Creates a new API interaction object.
+   *
+   * @param string  API token.
+   * @param string  API protocol and host.
+   */
+  public function __construct($auth_token, $api_target = self::DEFAULT_TARGET) {
+    $this->authToken = $auth_token;
+    $this->apiTarget = $api_target;
+  }
+
+
+/* -(  Rooms  )-------------------------------------------------------------- */
+
+  /**
+   * Get information about a room.
+   *
+   * @param  wild
+   * @return wild
+   *
+   * @see http://api.hipchat.com/docs/api/method/rooms/show
+   */
+  public function getRoom($room_id) {
+    $response = $this->makeRequest('rooms/show', array(
+      'room_id' => $room_id,
+    ));
+    return idx($response, 'room');
+  }
+
+  /**
+   * Determine if the given room name or room ID already exists.
+   *
+   * @param  string
+   * @return boolean
+   */
+  public function roomExists($room_id) {
+    try {
+      $this->getRoom($room_id);
+    } catch (HTTPFutureHTTPResponseStatus $ex) {
+      if ($ex->getStatusCode() === 404) {
+        return false;
+      } else {
+        throw $ex;
+      }
+    }
+    return true;
+  }
+
+  /**
+   * Get list of rooms
+   *
+   * @return list<map<string, wild>>
+   *
+   * @see http://api.hipchat.com/docs/api/method/rooms/list
+   */
+  public function getRooms() {
+    $response = $this->makeRequest('rooms/list');
+    return idx($response, 'rooms');
+  }
+
+  /**
+   * Send a message to a room.
+   *
+   * @param  wild
+   * @param  string
+   * @param  string
+   * @param  bool
+   * @param  const
+   * @param  const
+   * @return bool
+   *
+   * @see http://api.hipchat.com/docs/api/method/rooms/message
+   */
+  public function messageRoom(
+    $room_id,
+    $from,
+    $message,
+    $notify = false,
+    $color = self::COLOR_YELLOW,
+    $message_format = self::FORMAT_HTML) {
+
+    $args = array(
+      'room_id' => $room_id,
+      'from' => $from,
+      'message' => $message,
+      'notify' => (int)$notify,
+      'color' => $color,
+      'message_format' => $message_format,
+    );
+    $response = $this->makeRequest('rooms/message', $args, 'POST');
+    return ($response->status == 'sent');
+  }
+
+  /**
+   * Get chat history for a room.
+   *
+   * @param  int
+   * @param  string
+   * @return list<map<string, wild>>
+   *
+   * @see https://www.hipchat.com/docs/api/method/rooms/history
+   */
+   public function getRoomsHistory($room_id, $date = 'recent') {
+     $response = $this->makeRequest('rooms/history', array(
+      'room_id' => $room_id,
+      'date' => $date,
+     ));
+     return idx($response, 'messages');
+   }
+
+  /**
+   * Set a room's topic.
+   *
+   * @param  string
+   * @param  string
+   * @param  string
+   * @return bool
+   *
+   * @see http://api.hipchat.com/docs/api/method/rooms/topic
+   */
+   public function setRoomTopic($room_id, $topic, $from = null) {
+     $args = array(
+       'room_id' => $room_id,
+       'topic' => $topic,
+     );
+
+     if ($from) {
+       $args['from'] = $from;
+     }
+
+     $response = $this->makeRequest('rooms/topic', $args, 'POST');
+     return idx($response, 'status') == 'ok';
+   }
+
+
+/* -(  Users  )-------------------------------------------------------------- */
+
+  /**
+   * Get information about a user.
+   *
+   * @param  int
+   * @return map<string, wild>
+   *
+   * @see http://api.hipchat.com/docs/api/method/users/show
+   */
+  public function getUser($user_id) {
+    $response = $this->makeRequest('users/show', array(
+      'user_id' => $user_id,
+    ));
+    return idx($response, 'user');
+  }
+
+  /**
+   * Get list of users.
+   *
+   * @return list<map<string, wild>>
+   *
+   * @see http://api.hipchat.com/docs/api/method/users/list
+   */
+  public function getUsers() {
+    $response = $this->makeRequest('users/list');
+    return idx($response, 'users');
+  }
+
+
+/* -(  Utility  )------------------------------------------------------------ */
+
+  /**
+   * Make an API request.
+   *
+   * @param  string
+   * @param  map<string, wild>
+   * @param  string
+   * @return wild
+   */
+  private function makeRequest(
+    $api_method,
+    $args = array(),
+    $http_method = 'GET') {
+
+    $args['auth_token'] = $this->authToken;
+    $args['format'] = 'json';
+
+    $uri = new PhutilURI($this->apiTarget.$api_method);
+    $uri->setQueryParams($args);
+
+    $future = id(new HTTPSFuture($uri))
+      ->setMethod($http_method);
+
+    list($data, $headers) = $future->resolvex();
+    return phutil_json_decode($data, true);
+  }
+
+}

commit ce153f689e0a43b76ab6edfdc38444159d3600fc

Fix a method call
This was broken by D16981.

src/applications/herald/extension/HeraldHipChatNotificationCustomAction.php
@@ -56,7 +56,7 @@ final class HeraldHipChatNotificationCustomAction extends HeraldCustomAction {
     try {
       $client = $this->getClient();
 
-      $client->message_room(
+      $client->messageRoom(
         $effect->getTarget(),
         PhabricatorEnv::getEnvConfig('hipchat.author'),
         (string) $this->getMessage(