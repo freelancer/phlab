e3ffebd91297be39b85d84b3bc2fb8d60dc1629c
Joshua Spence <josh@joshuaspence.com>
2015-08-04 08:20:10 +1000

    Modernize HipChat Herald integration
    See https://secure.phabricator.com/T8726.
    
    Auditors: raymond.huang

--- src/__phutil_library_map__.php
+++ src/__phutil_library_map__.php
@@ -10,7 +10,7 @@ phutil_register_library_map(array(
   '__library_version__' => 2,
   'class' => array(
     'Composer' => 'infrastructure/Composer.php',
-    'HeraldHipChatNotificationCustomAction' => 'applications/herald/extension/HeraldHipChatNotificationCustomAction.php',
+    'HeraldHipChatNotificationAction' => 'applications/herald/extension/HeraldHipChatNotificationAction.php',
     'HipChatClient' => 'infrastructure/HipChatClient.php',
     'HipChatConfigOptions' => 'applications/config/option/HipChatConfigOptions.php',
     'PhlabAWSFileStorageEngine' => 'applications/files/engine/PhlabAWSFileStorageEngine.php',
@@ -20,7 +20,7 @@ phutil_register_library_map(array(
   'function' => array(),
   'xmap' => array(
     'Composer' => 'Phobject',
-    'HeraldHipChatNotificationCustomAction' => 'HeraldCustomAction',
+    'HeraldHipChatNotificationAction' => 'HeraldAction',
     'HipChatClient' => 'Phobject',
     'HipChatConfigOptions' => 'PhabricatorApplicationConfigOptions',
     'PhlabAWSFileStorageEngine' => 'PhabricatorFileStorageEngine',
---
+++ src/applications/herald/extension/HeraldHipChatNotificationAction.php
@@ -0,0 +1,131 @@
+<?php
+
+/**
+ * A Herald action for sending notifications to HipChat rooms.
+ */
+final class HeraldHipChatNotificationAction extends HeraldAction {
+
+  const ACTIONCONST = 'hipchat.notification';
+
+  const DO_FAILED = 'do.failed';
+  const DO_NOTIFY = 'do.notify';
+
+  public function getHeraldActionName() {
+    return pht('Notify a HipChat room');
+  }
+
+  public function supportsObject($object) {
+    if ($object instanceof DifferentialRevision) {
+      return true;
+    } else if ($object instanceof ManiphestTask) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public function supportsRuleType($rule_type) {
+    return $rule_type == HeraldRuleTypeConfig::RULE_TYPE_GLOBAL;
+  }
+
+  public function applyEffect($object, HeraldEffect $effect) {
+    $handle = id(new PhabricatorHandleQuery())
+      ->setViewer(PhabricatorUser::getOmnipotentUser())
+      ->withPHIDs(array($this->getAdapter()->getPHID()))
+      ->executeOne();
+
+    $action = $this->getAdapter()->getIsNewObject()
+      ? pht('New')
+      : pht('Updated');
+
+    try {
+      $this->getClient()->messageRoom(
+        $effect->getTarget(),
+        PhabricatorEnv::getEnvConfig('hipchat.author'),
+        (string)$this->getMessage(
+          $action,
+          pht('%s: %s', $object->getMonogram(), $object->getTitle()),
+          $handle),
+        false,
+        PhabricatorEnv::getEnvConfig('hipchat.color'));
+      $this->logEffect(self::DO_NOTIFY, $effect->getTarget());
+    } catch (Exception $ex) {
+      $this->logEffect(self::DO_FAILED, $ex->getMessage());
+    }
+  }
+
+  protected function renderActionEffectDescription($type, $data) {
+    switch ($type) {
+      case self::DO_FAILED:
+        return pht('Failed to notify the "%s" room.', $data);
+
+      case self::DO_NOTIFY:
+        return pht('Notified the "%s" room.', $data);
+    }
+  }
+
+  public function getActionGroupKey() {
+    return HeraldNotifyActionGroup::ACTIONGROUPKEY;
+  }
+
+  public function getHeraldActionStandardType() {
+    return self::STANDARD_TEXT;
+  }
+
+  protected function getActionEffectMap() {
+    return array(
+      self::DO_FAILED => array(
+        'icon' => 'fa-times',
+        'color' => 'red',
+        'name' => pht('Notification Failed'),
+      ),
+      self::DO_NOTIFY => array(
+        'icon' => 'fa-envelope',
+        'color' => 'green',
+        'name' => pht('Notified'),
+      ),
+    );
+  }
+
+  /**
+   * Create a new HipChat API object.
+   */
+  protected function getClient() {
+    $server = PhabricatorEnv::getEnvConfig('hipchat.server');
+    $token  = PhabricatorEnv::getEnvConfig('hipchat.token');
+
+    if (!$token) {
+      throw new Exception(pht('No HipChat API token specified!'));
+    }
+
+    return new HipChatClient($token, $server);
+  }
+
+  /**
+   * Create the notification message.
+   *
+   * @param  string
+   * @param  string
+   * @param  PhabricatorObjectHandle
+   * @return string
+   */
+  private function getMessage(
+    $action,
+    $title,
+    PhabricatorObjectHandle $handle) {
+
+    $header = phutil_tag(
+      'div',
+      array(),
+      array(
+        phutil_tag('b', array(), $action.': '),
+        phutil_tag(
+          'a',
+          array('href' => PhabricatorEnv::getURI($handle->getURI())),
+          $title),
+      ));
+
+    return (string)phutil_tag('div', array(), $header);
+  }
+
+}
