4fc982713582394d9f60b91dd69ecc0ba8d939de
Joshua Spence <josh@freelancer.com>
2015-12-11 07:32:18 +1100

    Revert "Fix autoloader issue"
    Summary:
    This reverts commit cd07327495c55eebe8c4f697adebeef1e3d274af. This hack is no longer necessary after [[https://secure.phabricator.com/D14523 | D14523: Don't throw `PhutilMissingSymbolException` from `class_exists` or `interface_exists`]].
    
    NOTE: This diff is the same as D29148, but it should be fine to re-apply this after [[https://secure.phabricator.com/D14860 | D29148: Don't throw `PhutilMissingSymbolException` from within `method_exists` and `property_exists` calls]].
    
    Test Plan: N/A
    
    Reviewers: tpham
    
    Reviewed By: tpham
    
    Differential Revision: https://phabricator.freelancer.com/D29759

--- src/infrastructure/Composer.php
+++ src/infrastructure/Composer.php
@@ -5,8 +5,6 @@
  */
 final class Composer extends Phobject {
 
-  private static $initialized = false;
-
   /**
    * Register the Composer autoloader.
    *
@@ -17,12 +15,6 @@ final class Composer extends Phobject {
    */
   public static function registerAutoloader() {
     require_once dirname(__FILE__).'/../../vendor/autoload.php';
-
-    // Workaround for https://secure.phabricator.com/T1116. See T27208.
-    if (!self::$initialized) {
-      spl_autoload_register(function() {});
-      self::$initialized = true;
-    }
   }
 
 }
