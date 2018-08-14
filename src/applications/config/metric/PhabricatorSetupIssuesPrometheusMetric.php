4ac1fc6ededcaa7e4a1a84413373731f812bfdb8
Joshua Spence <josh@freelancer.com>
2018-08-14 16:58:21 +1000

    Minor improvement to `PhabricatorSetupIssuesPrometheusMetric`
    Ref T44824.

--- src/applications/config/metric/PhabricatorSetupIssuesPrometheusMetric.php
+++ src/applications/config/metric/PhabricatorSetupIssuesPrometheusMetric.php
@@ -16,31 +16,33 @@ final class PhabricatorSetupIssuesPrometheusMetric extends PhabricatorPrometheus
 
   public function getValues(): array {
     $checks = PhabricatorSetupCheck::loadAllChecks();
-    $values = [];
 
-    foreach ($checks as $check) {
-      $check->runSetupChecks();
+    return array_mergev(
+      array_map(
+        function (PhabricatorSetupCheck $check): array {
+          $check->runSetupChecks();
 
-      $class          = get_class($check);
-      $issues         = $check->getIssues();
-      $fatal_issues   = mfilter($issues, 'getIsFatal');
-      $ignored_issues = mfilter($issues, 'getIsIgnored');
+          $class          = get_class($check);
+          $issues         = $check->getIssues();
+          $fatal_issues   = mfilter($issues, 'getIsFatal');
+          $ignored_issues = mfilter($issues, 'getIsIgnored');
 
-      $values[] = [
-        count($fatal_issues),
-        [$class, 'fatal'],
-      ];
-      $values[] = [
-        count($ignored_issues),
-        [$class, 'ignored'],
-      ];
-      $values[] = [
-        count($issues) - count($fatal_issues) - count($ignored_issues),
-        [$class, 'warning'],
-      ];
-    }
-
-    return $values;
+          return [
+            [
+              count($fatal_issues),
+              [$class, 'fatal'],
+            ],
+            [
+              count($ignored_issues),
+              [$class, 'ignored'],
+            ],
+            [
+              count($issues) - count($fatal_issues) - count($ignored_issues),
+              [$class, 'warning'],
+            ],
+          ];
+        },
+        $checks));
   }
 
 }
