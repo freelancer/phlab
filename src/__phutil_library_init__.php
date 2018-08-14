f2ff0779c16933ae8ee368b6b0c4a8dfb137a6dd
Joshua Spence <josh@freelancer.com>
2018-08-15 09:17:24 +1000

    Fix a linter violation

--- src/__phutil_library_init__.php
+++ src/__phutil_library_init__.php
@@ -3,7 +3,7 @@
 phutil_register_library('phlab', __FILE__);
 
 // Register the Composer autoloader.
-if (!@include_once(__DIR__.'/../vendor/autoload.php')) {
+if (!@include_once __DIR__.'/../vendor/autoload.php') {
   throw new Exception(
     pht(
       'Dependencies are not installed. Please run `%s` to install dependencies.',
