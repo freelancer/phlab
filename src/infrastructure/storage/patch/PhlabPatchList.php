d334a8ee9e47b444c737e4d4a4e381d7dd8ab8b1
Joshua Spence <josh@freelancer.com>
2018-08-15 10:33:06 +1000

    Add some type hints

--- src/infrastructure/storage/patch/PhlabPatchList.php
+++ src/infrastructure/storage/patch/PhlabPatchList.php
@@ -2,11 +2,11 @@
 
 final class PhlabPatchList extends PhabricatorSQLPatchList {
 
-  public function getNamespace() {
+  public function getNamespace(): string {
     return 'phlab';
   }
 
-  public function getPatches() {
+  public function getPatches(): array {
     $root = dirname(phutil_get_library_root('phlab'));
     $patches = $this->buildPatchesFromDirectory(
       $root.'/resources/sql/autopatches/');
