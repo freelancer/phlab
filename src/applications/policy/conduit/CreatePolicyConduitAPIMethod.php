0f24db5503d8d13c911307e995acd4c05897094d
Joshua Spence <josh@freelancer.com>
2018-08-17 14:20:45 +1000

    Minor tweaks to `CreatePolicyConduitAPIMethod`

--- src/applications/policy/conduit/CreatePolicyConduitAPIMethod.php
+++ src/applications/policy/conduit/CreatePolicyConduitAPIMethod.php
@@ -1,62 +1,64 @@
 <?php
 
-// TODO: remove this after T21277. We probably won't need to create policies
-// via Conduit after we have migrated repository hosting.
-
+/**
+ * TODO: Remove this after T21277. We probably won't need to create policies
+ * via Conduit after we have migrated repository hosting.
+ */
 final class CreatePolicyConduitAPIMethod extends ConduitAPIMethod {
-  public function getAPIMethodName() {
+
+  public function getAPIMethodName(): string {
     return 'policy.create';
   }
 
-  public function getMethodDescription() {
+  public function getMethodDescription(): string {
     return pht('Create custom access control policy objects.');
   }
 
-  protected function defineParamTypes() {
-    return array(
+  protected function defineParamTypes(): array {
+    return [
       'objectType' => 'required string',
-      'default' => 'required string',
-      'policy' => 'required list<map<string, wild>>',
-    );
+      'default'    => 'required string',
+      'policy'     => 'required list<map<string, wild>>',
+    ];
   }
 
-  protected function defineReturnType() {
+  protected function defineReturnType(): string {
     return 'map<string, string>';
   }
 
-  protected function defineErrorTypes() {
-    return array(
+  protected function defineErrorTypes(): array {
+    return [
       'ERR-INVALID-PARAMETER' => pht('Missing or malformed parameter.'),
-    );
+    ];
   }
 
-  protected function execute(ConduitAPIRequest $request) {
+  protected function execute(ConduitAPIRequest $request): array {
     $actor = $request->getUser();
 
-    $action_options = array(
+    $action_options = [
       PhabricatorPolicy::ACTION_ALLOW => pht('Allow'),
-      PhabricatorPolicy::ACTION_DENY => pht('Deny'),
-    );
+      PhabricatorPolicy::ACTION_DENY  => pht('Deny'),
+    ];
 
     $object_type = $request->getValue('objectType');
     if ($object_type) {
-      $phid_types = PhabricatorPHIDType::getAllInstalledTypes(
-        $request->getUser());
+      $phid_types = PhabricatorPHIDType::getAllInstalledTypes($request->getUser());
       if (empty($phid_types[$object_type])) {
         return new Aphront404Response();
       }
+
       $object = $phid_types[$object_type]->newObject();
       if (!$object) {
-        throw id(new ConduitException('ERR-INVALID-PARAMETER'))
-          ->setErrorDescription(pht("Unknown objectType '%s'", $object_type));
+        throw (new ConduitException('ERR-INVALID-PARAMETER'))
+          ->setErrorDescription(pht("Unknown object type: '%s'", $object_type));
       }
     } else {
-      throw id(new ConduitException('ERR-INVALID-PARAMETER'))
-        ->setErrorDescription(pht('objectType is required.'));
+      throw (new ConduitException('ERR-INVALID-PARAMETER'))
+        ->setErrorDescription(pht('Object type is required.'));
     }
 
-    $rules = id(new PhutilClassMapQuery())
-      ->setAncestorClass('PhabricatorPolicyRule')
+    $rules = (new PhutilClassMapQuery())
+      ->setAncestorClass(PhabricatorPolicyRule::class)
       ->execute();
 
     foreach ($rules as $key => $rule) {
@@ -67,50 +69,48 @@ final class CreatePolicyConduitAPIMethod extends ConduitAPIMethod {
 
     $rules = msort($rules, 'getRuleOrder');
 
-    $default_rule = array(
+    $default_rule = [
       'action' => head_key($action_options),
-      'rule' => head_key($rules),
-      'value' => null,
-    );
+      'rule'   => head_key($rules),
+      'value'  => null,
+    ];
 
-    $policy = id(new PhabricatorPolicy())
-      ->setRules(array($default_rule))
+    $policy = (new PhabricatorPolicy())
+      ->setRules([$default_rule])
       ->setDefaultAction(PhabricatorPolicy::ACTION_DENY);
 
-    $root_id = celerity_generate_unique_node_id();
-
+    $root_id        = celerity_generate_unique_node_id();
     $default_action = $policy->getDefaultAction();
-    $rule_data = $policy->getRules();
+    $rule_data      = $policy->getRules();
+    $data           = $request->getValue('policy');
+    $rule_data      = array();
 
-    $data = $request->getValue('policy');
-
-    $rule_data = array();
     foreach ($data as $rule) {
       $action = idx($rule, 'action');
+
       switch ($action) {
         case 'allow':
         case 'deny':
           break;
         default:
-          throw id(new ConduitException('ERR-INVALID-PARAMETER'))
-            ->setErrorDescription(pht("Invalid action '%s'!", $action));
+          throw (new ConduitException('ERR-INVALID-PARAMETER'))
+            ->setErrorDescription(pht("Invalid action: '%s'!", $action));
       }
 
       $rule_class = idx($rule, 'rule');
       if (empty($rules[$rule_class])) {
-        throw id(new ConduitException('ERR-INVALID-PARAMETER'))
-          ->setErrorDescription(pht("Invalid rule class '%s'!", $rule_class));
+        throw (new ConduitException('ERR-INVALID-PARAMETER'))
+          ->setErrorDescription(pht("Invalid rule class: '%s'!", $rule_class));
       }
 
       $rule_obj = $rules[$rule_class];
+      $value    = $rule_obj->getValueForStorage(idx($rule, 'value'));
 
-      $value = $rule_obj->getValueForStorage(idx($rule, 'value'));
-
-      $rule_data[] = array(
+      $rule_data[] = [
         'action' => $action,
-        'rule' => $rule_class,
-        'value' => $value,
-      );
+        'rule'   => $rule_class,
+        'value'  => $value,
+      ];
     }
 
     // Filter out nonsense rules, like a "users" rule without any users
@@ -124,7 +124,7 @@ final class CreatePolicyConduitAPIMethod extends ConduitAPIMethod {
     }
 
     if (!$valid_rules) {
-      throw id(new ConduitException('ERR-INVALID-PARAMETER'))
+      throw (new ConduitException('ERR-INVALID-PARAMETER'))
         ->setErrorDescription(pht('Rules do not have any effect.'));
     }
 
@@ -132,18 +132,19 @@ final class CreatePolicyConduitAPIMethod extends ConduitAPIMethod {
     // policy here. If we didn't, we would need to lock this endpoint down,
     // as users could otherwise just go edit the policies of objects with
     // custom policies.
-    $data = array();
+    $data = [];
 
-    $new_policy = new PhabricatorPolicy();
-    $new_policy->setRules($valid_rules);
-    $new_policy->setDefaultAction($request->getValue('default'));
-    $new_policy->save();
+    $new_policy = (new PhabricatorPolicy())
+      ->setRules($valid_rules)
+      ->setDefaultAction($request->getValue('default'))
+      ->save();
 
-    return array(
+    return [
       'phid' => $new_policy->getPHID(),
       'name' => $new_policy->getName(),
       'full' => $new_policy->getName(),
       'icon' => $new_policy->getIcon(),
-    );
+    ];
   }
+
 }
