96c93c31a6312fabcad598447bf0fb89635d02c3
Joshua Spence <josh@freelancer.com>
2018-10-23 17:22:19 +1100

    Customize transaction and feed text
    Summary: Ref T49816. Depends on D111488.
    
    Test Plan:
    = Before =
    {F776539, size=full}
    
    = After =
    {F776540, size=full}
    
    Reviewers: jbeck, tpham, wjiang
    
    Reviewed By: jbeck
    
    Maniphest Tasks: T49816
    
    Differential Revision: https://phabricator.tools.flnltd.com/D111576

--- src/applications/project/edge/PhabricatorOwnedByProjectEdgeType.php
+++ src/applications/project/edge/PhabricatorOwnedByProjectEdgeType.php
@@ -12,4 +12,60 @@ final class PhabricatorOwnedByProjectEdgeType extends PhabricatorEdgeType {
     return true;
   }
 
+  public function getTransactionAddString($actor, $add_count, $add_edges): PhutilSafeHTML {
+    return pht(
+      '%s added %s owner project(s): %s.',
+      $actor,
+      $add_count,
+      $add_edges);
+  }
+
+  public function getTransactionRemoveString($actor, $remove_count, $remove_edges): PhutilSafeHTML {
+
+    return pht(
+      '%s removed %s owner project(s): %s.',
+      $actor,
+      $remove_count,
+      $remove_edges);
+  }
+
+  public function getTransactionEditString($actor, $total_count, $add_count, $add_edges, $remove_count, $remove_edges): PhutilSafeHTML {
+    return pht(
+      '%s edited owner projects(s), added %s: %s; removed %s: %s.',
+      $actor,
+      $add_count,
+      $add_edges,
+      $remove_count,
+      $remove_edges);
+  }
+
+  public function getFeedAddString($actor, $object, $add_count, $add_edges): PhutilSafeHTML {
+    return pht(
+      '%s added %s owner project(s) for %s: %s.',
+      $actor,
+      $add_count,
+      $object,
+      $add_edges);
+  }
+
+  public function getFeedRemoveString($actor, $object, $remove_count, $remove_edges): PhutilSafeHTML {
+    return pht(
+      '%s removed %s owner project(s) for %s: %s.',
+      $actor,
+      $remove_count,
+      $object,
+      $remove_edges);
+  }
+
+  public function getFeedEditString($actor, $object, $total_count, $add_count, $add_edges, $remove_count, $remove_edges): PhutilSafeHTML {
+    return pht(
+      '%s edited owner project(s) for %s, added %s: %s; removed %s: %s.',
+      $actor,
+      $object,
+      $add_count,
+      $add_edges,
+      $remove_count,
+      $remove_edges);
+  }
+
 }
--- src/applications/project/edge/PhabricatorOwnsProjectEdgeType.php
+++ src/applications/project/edge/PhabricatorOwnsProjectEdgeType.php
@@ -12,4 +12,60 @@ final class PhabricatorOwnsProjectEdgeType extends PhabricatorEdgeType {
     return true;
   }
 
+  public function getTransactionAddString($actor, $add_count, $add_edges): PhutilSafeHTML {
+    return pht(
+      '%s added %s owned project(s): %s.',
+      $actor,
+      $add_count,
+      $add_edges);
+  }
+
+  public function getTransactionRemoveString($actor, $remove_count, $remove_edges): PhutilSafeHTML {
+
+    return pht(
+      '%s removed %s owned project(s): %s.',
+      $actor,
+      $remove_count,
+      $remove_edges);
+  }
+
+  public function getTransactionEditString($actor, $total_count, $add_count, $add_edges, $remove_count, $remove_edges): PhutilSafeHTML {
+    return pht(
+      '%s edited owned projects(s), added %s: %s; removed %s: %s.',
+      $actor,
+      $add_count,
+      $add_edges,
+      $remove_count,
+      $remove_edges);
+  }
+
+  public function getFeedAddString($actor, $object, $add_count, $add_edges): PhutilSafeHTML {
+    return pht(
+      '%s added %s owned project(s) for %s: %s.',
+      $actor,
+      $add_count,
+      $object,
+      $add_edges);
+  }
+
+  public function getFeedRemoveString($actor, $object, $remove_count, $remove_edges): PhutilSafeHTML {
+    return pht(
+      '%s removed %s owned project(s) for %s: %s.',
+      $actor,
+      $remove_count,
+      $object,
+      $remove_edges);
+  }
+
+  public function getFeedEditString($actor, $object, $total_count, $add_count, $add_edges, $remove_count, $remove_edges): PhutilSafeHTML {
+    return pht(
+      '%s edited owned project(s) for %s, added %s: %s; removed %s: %s.',
+      $actor,
+      $object,
+      $add_count,
+      $add_edges,
+      $remove_count,
+      $remove_edges);
+  }
+
 }
