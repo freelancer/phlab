097db8950ff113e1308ca2824e9d58fccdf368a5
Joshua Spence <josh@freelancer.com>
2018-11-12 13:17:38 +1100

    Add a `bin/files reset-view-policy` workflow
    Summary:
    Ref T69612. Ref T69782. The [[https://phabricator.tools.flnltd.com/files/ | Files application]] doesn't support [[https://phabricator.tools.flnltd.com/spaces/ | Spaces]] and so to reduce the risk of accidentally disclosing private information to our enterprise customers we are going to change the default view policy for files from "all users" to "internal users". Unfortunately, this setting doesn't apply retroactively and so all existing files created using the default view policy will be viewable by enterprise customers. This diff adds a `bin/files reset-view-policy` workflow that can be used to bulk modify the view policy of files.
    
    This isn't perfect because there's no way of knowing whether a file is actually using the default view policy or if the view policy was explicitly set to "all users", but it seems better that some files end up being more restricted than intended rather than files ending up less restricted than intended.
    
    Test Plan:
    Ran `/usr/local/src/phabricator/bin/files reset-view-policy --all --from users --to PHID-PLCY-vckfl6limrrgvotx4mhy` and saw the view policy of [[https://phabricator.tools-stg.flnltd.com/F931552 | F931552]] changed. Also tested a bunch of edge cases.
    
    = Edge cases =
    
    == With missing arguments ==
    ```name="/usr/local/src/phabricator/bin/files reset-view-policy --all", counterexample
    Usage Exception: You must specify a policy with `--from`.
    ```
    
    == With invalid policy identifier ==
    ```name="/usr/local/src/phabricator/bin/files reset-view-policy --all --from derp", counterexample
    Usage Exception: derp does not refer to a valid policy.
    ```
    
    Reviewers: jbeck, wjiang, tpham
    
    Reviewed By: jbeck, wjiang, tpham
    
    Maniphest Tasks: T69612, T69782
    
    Differential Revision: https://phabricator.tools.flnltd.com/D112859

---
+++ src/applications/files/management/PhabricatorFilesManagementResetViewPolicyWorkflow.php
@@ -0,0 +1,140 @@
+<?php
+
+final class PhabricatorFilesManagementResetViewPolicyWorkflow
+  extends PhabricatorFilesManagementWorkflow {
+
+  protected function didConstruct(): void {
+    $this
+      ->setName('reset-view-policy')
+      ->setSynopsis(pht('Reset file(s) to the specified view policy.'))
+      ->setArguments([
+        [
+          'name' => 'all',
+          'help' => pht('Reset the view policy on all files.'),
+        ],
+        [
+          'name' => 'dry-run',
+          'help' => pht('Show planned writes but do not perform them.'),
+        ],
+        [
+          'name'  => 'from',
+          'param' => 'from',
+          'help'  => pht('The previous view policy.'),
+        ],
+        [
+          'name'  => 'to',
+          'param' => 'to',
+          'help'  => pht('The new view policy.'),
+        ],
+        [
+          'name'     => 'names',
+          'wildcard' => true,
+        ],
+      ]);
+  }
+
+  public function execute(PhutilArgumentParser $args): int {
+    $iterator = $this->buildIterator($args);
+    if (!$iterator) {
+      throw new PhutilArgumentUsageException(
+        pht(
+          'Either specify a list of files or specify `%s` '.
+          'to reset the view policy on all files.',
+          '--all'));
+    }
+
+    $from_policy = $this->validatePolicy($args, 'from');
+    $to_policy   = $this->validatePolicy($args, 'to');
+    $is_dry_run  = $args->getArg('dry-run');
+
+    foreach ($iterator as $file) {
+      $view_policy = $file->getViewPolicy();
+
+      if ($view_policy !== $from_policy) {
+        $this->logInfo(
+          pht('SKIP'),
+          pht('%s has a custom view policy.', $file->getMonogram()));
+        continue;
+      }
+
+      if ($is_dry_run) {
+        $this->logOkay(
+          pht('DRY RUN'),
+          pht(
+            'View policy for %s would be modified ("%s" -> "%s").',
+            $file->getMonogram(),
+            $view_policy,
+            $to_policy));
+        continue;
+      }
+
+      try {
+        $file->setViewPolicy($to_policy);
+        $file->save();
+
+        $this->logOkay(
+          pht('MODIFIED'),
+          pht('View policy for %s has been reset.', $file->getMonogram()));
+      } catch (Exception $ex) {
+        $this->logWarn(
+          pht('WARN'),
+          pht(
+            'Failed to update view policy for %s: %s',
+            $file->getMonogram(),
+            $ex->getMessage()));
+      }
+    }
+
+    return 0;
+  }
+
+  /**
+   * Validate a policy identifier.
+   *
+   * A valid policy identifier is a PHID corresponding to a `PhabricatorPolicy`
+   * or one of the following constants:
+   *
+   *   - `PhabricatorPolicies::POLICY_PUBLIC`
+   *   - `PhabricatorPolicies::POLICY_USER`
+   *   - `PhabricatorPolicies::POLICY_ADMIN`
+   *   - `PhabricatorPolicies::POLICY_NOONE`
+   *
+   * @param  PhutilArgumentParser
+   * @param  string
+   * @return string
+   */
+  private function validatePolicy(PhutilArgumentParser $args, string $arg): string {
+    $policy_id = $args->getArg($arg);
+
+    if ($policy_id === null) {
+      throw new PhutilArgumentUsageException(
+        pht(
+          'You must specify a policy with `%s`.',
+          '--'.$arg));
+    }
+
+    switch ($policy_id) {
+      case PhabricatorPolicies::POLICY_PUBLIC:
+      case PhabricatorPolicies::POLICY_USER:
+      case PhabricatorPolicies::POLICY_ADMIN:
+      case PhabricatorPolicies::POLICY_NOONE:
+        return $policy_id;
+
+      default:
+        $policy = (new PhabricatorPolicyQuery())
+          ->setViewer($this->getViewer())
+          ->withPHIDs([$policy_id])
+          ->executeOne();
+
+        if ($policy === null) {
+          throw new PhutilArgumentUsageException(
+            pht(
+              '%s does not refer to a valid policy.',
+              $policy_id));
+        }
+
+        return $policy_id;
+    }
+  }
+
+}
