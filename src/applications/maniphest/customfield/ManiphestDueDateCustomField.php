ebcad75ea9761ca7e04e9f306db54dff4cb88836
Joshua Spence <josh@freelancer.com>
2019-01-17 10:50:07 +1100

    Add "Due Date" custom field
    Summary: Adds a Maniphest custom field that can be used for setting the due date for a task.
    
    Test Plan: [[https://phabricator.tools-stg.flnltd.com/T430 | Tested in staging]].
    
    Reviewers: jbeck, tpham, wjiang
    
    Reviewed By: jbeck
    
    Differential Revision: https://phabricator.tools.flnltd.com/D117277

---
+++ src/applications/maniphest/customfield/ManiphestDueDateCustomField.php
@@ -0,0 +1,128 @@
+<?php
+
+final class ManiphestDueDateCustomField extends ManiphestCustomField {
+
+  private $value;
+
+  public function getFieldKey(): string {
+    return 'maniphest:due-date';
+  }
+
+  public function getFieldName(): string {
+    return pht('Due Date');
+  }
+
+  public function shouldUseStorage(): bool {
+    return true;
+  }
+
+  public function getValueForStorage(): ?int {
+    return $this->value;
+  }
+
+  public function setValueFromStorage($value): void {
+    $this->value = $value;
+  }
+
+  public function shouldAppearInApplicationTransactions(): bool {
+    return true;
+  }
+
+  public function getApplicationTransactionTitle(PhabricatorApplicationTransaction $xaction): PhutilSafeHTML {
+    $author_phid = $xaction->getAuthorPHID();
+    $viewer      = $this->getViewer();
+
+    $old_value = $xaction->getOldValue();
+    $new_value = $xaction->getNewValue();
+
+    if ($old_value === null) {
+      return pht(
+        '%s set %s to %s.',
+        $xaction->renderHandleLink($author_phid),
+        $this->getFieldName(),
+        phabricator_datetime($new_value, $viewer));
+    } else if ($new_value === null) {
+      return pht(
+        '%s removed %s.',
+        $xaction->renderHandleLink($author_phid),
+        $this->getFieldName());
+    } else {
+      return pht(
+        '%s changed %s from %s to %s.',
+        $xaction->renderHandleLink($author_phid),
+        $this->getFieldName(),
+        phabricator_datetime($old_value, $viewer),
+        phabricator_datetime($new_value, $viewer));
+    }
+  }
+
+  public function getApplicationTransactionTitleForFeed(PhabricatorApplicationTransaction $xaction): PhutilSafeHTML {
+    $author_phid = $xaction->getAuthorPHID();
+    $object_phid = $xaction->getObjectPHID();
+    $viewer      = $this->getViewer();
+
+    $old_value = $xaction->getOldValue();
+    $new_value = $xaction->getNewValue();
+
+    if ($old_value === null) {
+      return pht(
+        '%s set %s to %s on %s.',
+        $xaction->renderHandleLink($author_phid),
+        $this->getFieldName(),
+        phabricator_datetime($new_value, $viewer),
+        $xaction->renderHandleLink($object_phid));
+    } else if ($new_value === null) {
+      return pht(
+        '%s removed %s on %s.',
+        $xaction->renderHandleLink($author_phid),
+        $this->getFieldName(),
+        $xaction->renderHandleLink($object_phid));
+    } else {
+      return pht(
+        '%s changed %s from %s to %s on %s.',
+        $xaction->renderHandleLink($author_phid),
+        $this->getFieldName(),
+        phabricator_datetime($old_value, $viewer),
+        phabricator_datetime($new_value, $viewer),
+        $xaction->renderHandleLink($object_phid));
+    }
+  }
+
+  public function readValueFromRequest(AphrontRequest $request): void {
+    $control = $this->newDateControl();
+    $control->setUser($request->getUser());
+
+    $this->value = $control->readValueFromRequest($request);
+  }
+
+  public function renderEditControl(array $handles): AphrontFormControl {
+    return $this->newDateControl();
+  }
+
+  public function shouldAppearInPropertyView(): bool {
+    return true;
+  }
+
+  public function renderPropertyViewValue(array $handles): ?string {
+    if ($this->value === null) {
+      return null;
+    }
+
+    return phabricator_datetime($this->value, $this->getViewer());
+  }
+
+  /**
+   * NOTE: This method was largely copied from
+   * @{method:PhabricatorStandardCustomFieldDate::newDateControl}.
+   */
+  private function newDateControl(): AphrontFormDateControl {
+    return (new AphrontFormDateControl())
+      ->setViewer($this->getViewer())
+      ->setLabel($this->getFieldName())
+      ->setName($this->getFieldKey())
+      ->setValue($this->value)
+      ->setAllowNull(true)
+      ->setIsTimeDisabled(true);
+  }
+
+}
