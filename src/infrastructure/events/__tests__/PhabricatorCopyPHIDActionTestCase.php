a8707a4981cf105b0d5f21f3573bbb5a83694d1d
Joshua Spence <josh@freelancer.com>
2019-01-17 10:42:36 +1100

    Add unit tests for "Copy PHID" action
    Summary: Add basic unit tests for the "Copy PHID" action.
    
    Test Plan: Ran unit tests.
    
    Reviewers: jbeck, tpham, wjiang
    
    Reviewed By: jbeck
    
    Differential Revision: https://phabricator.tools.flnltd.com/D116832

---
+++ src/infrastructure/events/__tests__/PhabricatorCopyPHIDActionTestCase.php
@@ -0,0 +1,49 @@
+<?php
+
+final class PhabricatorCopyPHIDActionTestCase extends PhabricatorTestCase {
+
+  protected function getPhabricatorTestCaseConfiguration(): array {
+    return [
+      self::PHABRICATOR_TESTCONFIG_BUILD_STORAGE_FIXTURES => true,
+    ];
+  }
+
+  public function testHandleEventWithoutObject(): void {
+    $user  = $this->generateNewTestUser();
+    $event = $this->generateEvent($user);
+
+    $this->handleEvent($event);
+
+    $actions = $event->getValue('actions');
+    $this->assertTrue(empty($actions));
+  }
+
+  public function testHandleEventWithObject(): void {
+    $user = $this->generateNewTestUser();
+    $task = ManiphestTask::initializeNewTask($user)
+      ->save();
+
+    $event = $this->generateEvent($user)
+      ->setValue('object', $task);
+
+    $this->handleEvent($event);
+
+    $actions = $event->getValue('actions');
+    $this->assertEqual(1, count($actions));
+
+    $action = head($actions);
+    $this->assertEqual($user, $action->getViewer());
+    $this->assertEqual(['text' => $task->getPHID()], $action->getMetadata());
+  }
+
+  private function generateEvent(PhabricatorUser $user): PhabricatorEvent {
+    return (new PhabricatorEvent(PhabricatorEventType::TYPE_UI_DIDRENDERACTIONS))
+      ->setUser($user);
+  }
+
+  private function handleEvent(PhabricatorEvent $event): void {
+    $listener = new PhabricatorCopyPHIDAction();
+    $listener->handleEvent($event);
+  }
+
+}
