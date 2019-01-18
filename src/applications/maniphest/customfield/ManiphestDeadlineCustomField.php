68d4d4ce15ae4e085ea75a9604c7010b07535456
Joshua Spence <josh@freelancer.com>
2019-01-18 11:45:44 +1100

    Allow searching on the deadline field
    Summary: Index the deadline field and allow it to be used with application search. Specifically, allow searching for tasks with deadline before or after a specified time.
    
    Test Plan: Created a task with deadline set to yesterday. Searched for tasks with "Deadline Before" set to `now` and saw expected results.
    
    Reviewers: jbeck, tpham, wjiang
    
    Reviewed By: jbeck
    
    Differential Revision: https://phabricator.tools.flnltd.com/D117354

--- src/applications/maniphest/customfield/ManiphestDeadlineCustomField.php
+++ src/applications/maniphest/customfield/ManiphestDeadlineCustomField.php
@@ -24,6 +24,82 @@ final class ManiphestDeadlineCustomField extends ManiphestCustomField {
     $this->value = $value;
   }
 
+  public function shouldAppearInApplicationSearch(): bool {
+    return true;
+  }
+
+  public function buildFieldIndexes(): array {
+    $indexes = [];
+
+    if ($this->value !== null) {
+      $indexes[] = $this->newNumericIndex($this->value);
+    }
+
+    return $indexes;
+  }
+
+  public function buildOrderIndex(): ?ManiphestCustomFieldNumericIndex {
+    return $this->newNumericIndex(0);
+  }
+
+  public function readApplicationSearchValueFromRequest(PhabricatorApplicationSearchEngine $engine, AphrontRequest $request): array {
+    $key = $this->getFieldKey();
+
+    return [
+      'min' => $request->getStr($key.'.min'),
+      'max' => $request->getStr($key.'.max'),
+    ];
+  }
+
+  public function applyApplicationSearchConstraintToQuery(PhabricatorApplicationSearchEngine $engine, PhabricatorCursorPagedPolicyAwareQuery $query, $value): void {
+    $viewer = $this->getViewer();
+
+    if (!is_array($value)) {
+      $value = [];
+    }
+
+    $min_str = idx($value, 'min', '');
+    if (strlen($min_str)) {
+      $min = PhabricatorTime::parseLocalTime($min_str, $viewer);
+    } else {
+      $min = null;
+    }
+
+    $max_str = idx($value, 'max', '');
+    if (strlen($max_str)) {
+      $max = PhabricatorTime::parseLocalTime($max_str, $viewer);
+    } else {
+      $max = null;
+    }
+
+    if ($min !== null || $max !== null) {
+      $query->withApplicationSearchRangeConstraint(
+        $this->newNumericIndex(null),
+        $min,
+        $max);
+    }
+  }
+
+  public function appendToApplicationSearchForm(PhabricatorApplicationSearchEngine $engine, AphrontFormView $form, $value): void {
+    $key = $this->getFieldKey();
+
+    if (!is_array($value)) {
+      $value = [];
+    }
+
+    $form
+      ->appendChild(
+        (new AphrontFormTextControl())
+          ->setLabel(pht('Deadline After'))
+          ->setName($key.'.min')
+          ->setValue(idx($value, 'min')))
+      ->appendChild(
+        (new AphrontFormTextControl())
+          ->setLabel(pht('Deadline Before'))
+          ->setName($key.'.max')
+          ->setValue(idx($value, 'max')));
+  }
+
   public function shouldAppearInApplicationTransactions(): bool {
     return true;
   }
