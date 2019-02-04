5d86edf41b897c65a2931709ef14bbea523a09ea
Joshua Spence <josh@freelancer.com>
2019-02-04 12:53:04 +1100

    Add Docker files for local development
    Summary: Adds a basic `docker-compose.yaml` file that can be used to develop {rPHL} locally.
    
    Test Plan:
    # Ran `docker-compose up`.
    # Added `127.0.0.1 phabricator.local` to my local `/etc/hosts` file.
    # Accessed http://phabricator.local in a browser.
    
    Reviewers: dbunadi, tpham
    
    Reviewed By: dbunadi, tpham
    
    Differential Revision: https://phabricator.tools.flnltd.com/D130217

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Custom extensions for Phabricator.
+
+= Development =
+The best way to develop Phlab is to use [Docker Compose](https://docs.docker.com/compose/) locally by running `docker-compose up` from the root of the repository. Before doing so, you should have the following repositories cloned locally, in the same parent directory as Phlab itself:
+
+  - [Arcanist](https://github.com/phacility/arcanist)
+  - [libphutil](https://github.com/phacility/libphutil)
+  - [Phabricator](https://github.com/phacility/phabricator)
---
+++ support/docker/config.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+  'darkconsole.enabled'          => true,
+  'load-libraries'               => ['phlab/src'],
+  'metamta.default-address'      => 'noreply@'.getenv('PHABRICATOR_DOMAIN'),
+  'metamta.reply-handler-domain' => getenv('PHABRICATOR_DOMAIN'),
+  'mysql.host'                   => getenv('PHABRICATOR_MYSQL_HOST'),
+  'mysql.pass'                   => getenv('PHABRICATOR_MYSQL_PASSWORD'),
+  'mysql.user'                   => getenv('PHABRICATOR_MYSQL_USER'),
+  'phabricator.base-uri'         => 'http://'.getenv('PHABRICATOR_DOMAIN'),
+  'phabricator.developer-mode'   => true,
+  'phabricator.show-prototypes'  => true,
+  'phabricator.timezone'         => 'Etc/UTC',
+];
