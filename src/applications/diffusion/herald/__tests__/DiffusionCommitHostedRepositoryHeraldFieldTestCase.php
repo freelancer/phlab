d07bcb9d6346565a90f021cfa0d2700bec474af2
Joshua Spence <josh@freelancer.com>
2019-02-18 09:57:36 +1100

    Add basic unit tests for `DiffusionCommitHostedRepositoryHeraldField`

---
+++ src/applications/diffusion/herald/__tests__/DiffusionCommitHostedRepositoryHeraldFieldTestCase.php
@@ -0,0 +1,21 @@
+<?php
+
+final class DiffusionCommitHostedRepositoryHeraldFieldTestCase
+  extends PhutilTestCase {
+
+  public function testGetHeraldFieldValue(): void {
+    $repo = (new PhabricatorRepository());
+
+    $commit = (new PhabricatorRepositoryCommit())
+      ->attachRepository($repo);
+
+    $field = new DiffusionCommitHostedRepositoryHeraldField();
+
+    $repo->setHosted(true);
+    $this->assertTrue($field->getHeraldFieldValue($commit));
+
+    $repo->setHosted(false);
+    $this->assertFalse($field->getHeraldFieldValue($commit));
+  }
+
+}
