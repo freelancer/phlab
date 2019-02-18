78b0a341c2c8e647d0ce457c0c3cf16ab643c581
Joshua Spence <josh@freelancer.com>
2019-02-18 14:08:06 +1100

    Add `bin/repository find-orphaned-commits`
    Summary: Ref T77785. Adds a script to find orphaned commits in a repository (commits that don't appear on any branch).
    
    Test Plan:
    Applied the following patch (as otherwise the command takes a very long time to execute) and ran on rPP:
    
    ```lang=diff
    diff --git src/applications/repository/management/PhabricatorRepositoryManagementFindOrphanedCommitsWorkflow.php src/applications/repository/management/PhabricatorRepositoryManagementFindOrphanedCommitsWorkflow.php
    index 5d7c215..7090fd1 100644
    --- src/applications/repository/management/PhabricatorRepositoryManagementFindOrphanedCommitsWorkflow.php
    +++ src/applications/repository/management/PhabricatorRepositoryManagementFindOrphanedCommitsWorkflow.php
    @@ -50,6 +50,7 @@ final class PhabricatorRepositoryManagementFindOrphanedCommitsWorkflow
         $commits = (new DiffusionCommitQuery())
           ->setViewer($viewer)
           ->withRepository($repository)
    +      ->setLimit(1024)
           ->execute();
    
         $client  = $repository->newConduitClient($this->getViewer());
    ```
    
    The script correctly identifies the following commits:
    
      - {rPP76797c7529390389577c5b3b0a412d850411d97c}
      - {rPP61e1b535b2a88c0961af1a325a2431d6084139f6}
      - {rPP5e91cff41f5f7fe41496ef1906864a888e98dffb}
      - {rPP19d7da8bc27081eb8c863538da5f389ebcaba670}
    
    Reviewers: tpham, jbeck
    
    Reviewed By: tpham
    
    Maniphest Tasks: T77785
    
    Differential Revision: https://phabricator.tools.flnltd.com/D119533

---
+++ src/applications/repository/management/PhabricatorRepositoryManagementFindOrphanedCommitsWorkflow.php
@@ -0,0 +1,80 @@
+<?php
+
+final class PhabricatorRepositoryManagementFindOrphanedCommitsWorkflow
+  extends PhabricatorRepositoryManagementWorkflow {
+
+  protected function didConstruct(): void {
+    $this
+      ->setName('find-orphaned-commits')
+      ->setExamples('**find-orphaned-commits** [__options__] __repository__ ...')
+      ->setSynopsis(pht('List commits which are not found on any branch.'))
+      ->setArguments([
+        [
+          'name'     => 'repositories',
+          'wildcard' => true,
+        ],
+      ]);
+  }
+
+  public function execute(PhutilArgumentParser $args): int {
+    $console      = PhutilConsole::getConsole();
+    $repositories = $this->loadRepositories($args, 'repositories');
+
+    if (!$repositories) {
+      throw new PhutilArgumentUsageException(
+        pht('Specify one or more repositories.'));
+    }
+
+    foreach ($repositories as $repository) {
+      $orphaned_commits = $this->getOrphanedCommits($repository);
+
+      foreach ($orphaned_commits as $commit) {
+        $console->writeOut("%s\n", $commit);
+      }
+    }
+
+    return 0;
+  }
+
+  private function getOrphanedCommits(PhabricatorRepository $repository): iterable {
+    $viewer = $this->getViewer();
+
+    if (!$repository->isGit()) {
+      throw new PhutilArgumentUsageException(
+        pht(
+          'Only Git repositories are supported, this repository ("%s") '.
+          'is not a Git repository.',
+          $repository->getDisplayName()));
+    }
+
+    $commits = (new DiffusionCommitQuery())
+      ->setViewer($viewer)
+      ->withRepository($repository)
+      ->setLimit(1024)
+      ->execute();
+
+    $client  = $repository->newConduitClient($this->getViewer());
+    $futures = [];
+
+    foreach ($commits as $commit) {
+      $futures[$commit->getCommitIdentifier()] = $client->callMethod(
+        'diffusion.branchquery',
+        [
+          'contains'   => $commit->getCommitIdentifier(),
+          'repository' => $repository->getPHID(),
+        ]);
+    }
+
+    $futures = (new FutureIterator($futures))
+      ->limit(8);
+
+    foreach ($futures as $key => $future) {
+      $branches = $future->resolve();
+
+      if (!$branches) {
+        yield $key;
+      }
+    }
+  }
+
+}
