e6b1a6f2455656ed67216645c5af0ba925d0d029
Joshua Spence <josh@freelancer.com>
2019-03-14 12:57:09 +1100

    Change commit hook to make HTTP requests instead of running Harbormaster build plans
    Summary: Ref T21277. I originally planned for the `post-receive` commit hook to trigger build plans (specifically [[https://phabricator.tools.flnltd.com/harbormaster/plan/6/ | Build Plan 6]]), but it turns out that this won't work since that would require a `HarbormasterBuildable`, which we don't have since commits to non-tracked branches aren't persisted to the Phabricator database. Instead, I am changing the commit hook script to make HTTP requests to arbitrary URLs. Depends on D121641.
    
    Test Plan:
    Manually changed the commit hook for a repository:
    
    ```lang=bash
    #!/bin/sh
    /usr/local/src/phlab/scripts/repository/commit_hook.php --uri 'https://webhook.site/86afbc4b-8592-4074-8dd7-8c49d144cfb9?commit=${commit}'
    ```
    
    Pushed a commit to the repository and saw the request in the [[https://webhook.site/#/86afbc4b-8592-4074-8dd7-8c49d144cfb9/f246a148-4060-4ead-9163-bf277d765a6c/1 | Webhook.site UI]].
    
    Reviewers: jbeck, tpham
    
    Reviewed By: jbeck
    
    Maniphest Tasks: T21277
    
    Differential Revision: https://phabricator.tools.flnltd.com/D121643

--- scripts/repository/commit_hook.php
+++ scripts/repository/commit_hook.php
@@ -7,41 +7,21 @@ $args = new PhutilArgumentParser($argv);
 $args->setTagline(pht('Git post-receive hook'));
 $args->setSynopsis(<<<EOHELP
 **commit_hook.php** [__options__]
-    A Git post-receive hook that triggers a Harbormaster build plan for pushes
-    to non-tracked branches.
+    A Git post-receive hook that makes a HTTP GET request for pushes to
+    non-tracked branches.
 EOHELP
   );
 $args->parseStandardArguments();
 $args->parse(
   [
     [
-      'name'  => 'build-plan',
-      'param' => 'id',
-      'help'  => pht('ID of the Harbormaster build plan to be executed.'),
+      'name'  => 'uri',
+      'param' => 'template',
+      'help'  => pht('The URI to make a HTTP request to.'),
     ],
   ]);
 
 $console = PhutilConsole::getConsole();
-
-$username = getenv(DiffusionCommitHookEngine::ENV_USER) ?: null;
-if ($username === null) {
-  throw new PhutilArgumentUsageException(
-    pht(
-      '%s is not set!',
-      DiffusionCommitHookEngine::ENV_USER));
-}
-
-$user = (new PhabricatorPeopleQuery())
-  ->setViewer(PhabricatorUser::getOmnipotentUser())
-  ->withUsernames([$username])
-  ->executeOne();
-
-if ($user === null) {
-  throw new PhutilArgumentUsageException(
-    pht(
-      'No such user "%s"!',
-      $username));
-}
 
 // TODO: We //should// use `PhabricatorRepositoryQuery` here, but
 // `PhabricatorRepositoryQuery` doesn't expose a `withLocalPaths` method.
@@ -61,29 +41,13 @@ if ($repository === null) {
       $cwd));
 }
 
-// Don't do anything for non-hosted repositories.
-if (!$repository->isHosted()) {
+if (PhabricatorEnv::getEnvConfig('phabricator.silent')) {
   return 0;
 }
 
-$build_plan_id = $args->getArg('build-plan');
-if ($build_plan_id === null) {
-  throw new PhutilArgumentUsageException(
-    pht(
-      'You must specify a build plan with `%s`.',
-      '--build-plan'));
-}
-
-$build_plan = (new HarbormasterBuildPlanQuery())
-  ->setViewer($user)
-  ->withIDs([$build_plan_id])
-  ->executeOne();
-
-if ($build_plan === null) {
-  throw new PhutilArgumentUsageException(
-    pht(
-      'No such build plan (%d) was found!',
-      $build_plan_id));
+// Don't do anything for non-hosted repositories.
+if (!$repository->isHosted()) {
+  return 0;
 }
 
 $stdin = @file_get_contents('php://stdin');
@@ -105,42 +69,19 @@ foreach (phutil_split_lines($stdin, false) as $line) {
     continue;
   }
 
-  $commit = (new DiffusionCommitQuery())
-    ->setViewer($user)
-    ->withRepository($repository)
-    ->withIdentifiers([$new_commit])
-    ->executeOne();
+  // See @{method:PhabricatorRepositoryCommit::getBuildVariables}.
+  $variables = [
+    'branch'         => $branch,
+    'commit'         => $new_commit,
+    'repository.uri' => $repository->getPublicCloneURI(),
+  ];
 
-  // TODO: Add retry logic.
-  if ($commit === null) {
-    $console->writeErr(
-      "%s\n",
-      pht(
-        'Commit %s has not yet been processed by daemons. '.
-        'Unable to trigger Harbormaster build plan %d.',
-        $new_commit,
-        $build_plan->getID()));
-    continue;
-  }
+  $uri = varsprintf('vurisprintf', $args->getArg('uri'), $variables);
+  $uri = new PhutilURI($uri);
 
-  $buildable = HarbormasterBuildable::initializeNewBuildable($user)
-    ->setBuildablePHID($commit->getHarbormasterBuildablePHID())
-    ->setContainerPHID($commit->getHarbormasterContainerPHID())
-    ->save();
+  $future = (new HTTPSFuture($uri))
+    ->setMethod('GET')
+    ->setTimeout(10);
 
-  $buildable->sendMessage(
-    $user,
-    HarbormasterMessageType::BUILDABLE_BUILD,
-    false);
-
-  $console->writeOut(
-    "%s\n\n    %s\n\n",
-    pht(
-      'Applying plan %s to new buildable %s...',
-      $build_plan->getID(),
-      $buildable->getMonogram()),
-    PhabricatorEnv::getProductionURI('/'.$buildable->getMonogram()));
-
-  $build = $buildable->applyPlan($build_plan, [], $user->getPHID());
-  $console->writeOut("%s\n", pht('Done.'));
+  $future->resolvex();
 }
