2c497f380bbc3dedf3360eda8a2c792d8195d182
Joshua Spence <josh@freelancer.com>
2019-04-15 11:01:46 +1000

    Minor reordering

--- src/infrastructure/internationalization/translation/PhlabUSEEnglishTranslation.php
+++ src/infrastructure/internationalization/translation/PhlabUSEEnglishTranslation.php
@@ -8,36 +8,6 @@ final class PhlabUSEnglishTranslation extends PhutilTranslation {
 
   protected function getTranslations(): array {
     return [
-      // PhabricatorOwnsProjectEdgeType
-      '%s added %s owned project(s): %s.' => [
-        [
-          '%s added owned project: %3$s.',
-          '%s added %s owned projects: %s.',
-        ],
-      ],
-      '%s added %s owned project(s) for %s: %s.' => [
-        [
-          '%s added owned project for %3$s: %4$s.',
-          '%s added %s owned projects for %s: %s.',
-        ],
-      ],
-      '%s edited owned project(s), added %s: %s; removed %s: %s.' =>
-        '%s edited owned projects, added %3$s; removed %5$s.',
-      '%s edited owned project(s) for %s, added %s: %s; removed %s: %s.' =>
-        '%s edited owned projects for %s, added %4$s; removed: %6$s.',
-      '%s removed %s owned project(s): %s.' => [
-        [
-          '%s removed owned project: %3$s.',
-          '%s removed %s owned projects: %s.',
-        ],
-      ],
-      '%s removed %s owned project(s) for %s: %s.' => [
-        [
-          '%s removed owned project for %3$s: %4$s.',
-          '%s removed %s owned projects for %s: %s.',
-        ],
-      ],
-
       // PhabricatorOwnedByProjectEdgeType
       '%s added %s owner project(s): %s.' => [
         [
@@ -65,6 +35,36 @@ final class PhlabUSEnglishTranslation extends PhutilTranslation {
         [
           '%s removed owner project for %3$s: %4$s.',
           '%s removed %s owner projects for %s: %s.',
+        ],
+      ],
+
+      // PhabricatorOwnsProjectEdgeType
+      '%s added %s owned project(s): %s.' => [
+        [
+          '%s added owned project: %3$s.',
+          '%s added %s owned projects: %s.',
+        ],
+      ],
+      '%s added %s owned project(s) for %s: %s.' => [
+        [
+          '%s added owned project for %3$s: %4$s.',
+          '%s added %s owned projects for %s: %s.',
+        ],
+      ],
+      '%s edited owned project(s), added %s: %s; removed %s: %s.' =>
+        '%s edited owned projects, added %3$s; removed %5$s.',
+      '%s edited owned project(s) for %s, added %s: %s; removed %s: %s.' =>
+        '%s edited owned projects for %s, added %4$s; removed: %6$s.',
+      '%s removed %s owned project(s): %s.' => [
+        [
+          '%s removed owned project: %3$s.',
+          '%s removed %s owned projects: %s.',
+        ],
+      ],
+      '%s removed %s owned project(s) for %s: %s.' => [
+        [
+          '%s removed owned project for %3$s: %4$s.',
+          '%s removed %s owned projects for %s: %s.',
         ],
       ],
     ];
