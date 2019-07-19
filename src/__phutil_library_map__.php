e92605cd6fdf92378dca44822bc3f7e88ddf4ee9
Joshua Spence <josh@freelancer.com>
2019-07-19 14:58:45 +1000

    Add "date range" form control
    Summary: Adds `AphrontFormDateRangeControl`, which is a form control that allows selecting a date range. Replaces D119353.
    
    Test Plan: Tested as a part of D131859.
    
    Reviewers: awright, tpham
    
    Reviewed By: tpham
    
    Differential Revision: https://phabricator.tools.flnltd.com/D131590

--- src/__phutil_library_map__.php
+++ src/__phutil_library_map__.php
@@ -9,6 +9,7 @@
 phutil_register_library_map(array(
   '__library_version__' => 2,
   'class' => array(
+    'AphrontFormDateRangeControl' => 'view/form/control/AphrontFormDateRangeControl.php',
     'DiffusionChangesConduitAPIMethod' => 'applications/diffusion/conduit/DiffusionChangesConduitAPIMethod.php',
     'DiffusionChangesConduitAPIMethodTestCase' => 'applications/diffusion/conduit/__tests__/DiffusionChangesConduitAPIMethodTestCase.php',
     'DiffusionCommitHostedRepositoryHeraldField' => 'applications/diffusion/herald/DiffusionCommitHostedRepositoryHeraldField.php',
@@ -68,6 +69,7 @@ phutil_register_library_map(array(
     'varsprintf' => 'utils/utils.php',
   ),
   'xmap' => array(
+    'AphrontFormDateRangeControl' => 'AphrontFormControl',
     'DiffusionChangesConduitAPIMethod' => 'DiffusionQueryConduitAPIMethod',
     'DiffusionChangesConduitAPIMethodTestCase' => 'PhutilTestCase',
     'DiffusionCommitHostedRepositoryHeraldField' => 'DiffusionCommitHeraldField',
---
+++ src/view/form/control/AphrontFormDateRangeControl.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * A form control which proxies @{class:AphrontFormDateControl} to allow
+ * defining a date range.
+ */
+final class AphrontFormDateRangeControl extends AphrontFormControl {
+
+  private $startControl;
+  private $endControl;
+
+  public function __construct() {
+    $this->startControl = new AphrontFormDateControl();
+    $this->endControl   = new AphrontFormDateControl();
+  }
+
+  public function __clone() {
+    $this->startControl = clone $this->startControl;
+    $this->endControl   = clone $this->endControl;
+  }
+
+  public function setIsTimeDisabled($is_disabled) {
+    $this->startControl->setIsTimeDisabled($is_disabled);
+    $this->endControl->setIsTimeDisabled($is_disabled);
+    return $this;
+  }
+
+  public function setViewer(PhabricatorUser $viewer) {
+    parent::setViewer($viewer);
+    $this->startControl->setViewer($viewer);
+    $this->endControl->setViewer($viewer);
+    return $this;
+  }
+
+  public function setName($name) {
+    parent::setName($name);
+
+    // We need `$this->startControl` and `$this->endControl` to have unique
+    // names as these names are used for the input names (see
+    // @{method:AphrontFormDateControl::getDateInputName}).
+    $this->startControl->setName($name.'_start');
+    $this->endControl->setName($name.'_end');
+
+    return $this;
+  }
+
+  public function setValue($value) {
+    // TODO: We should check that `$value` is of the expected type.
+    $this->startControl->setValue($value[0]);
+    $this->endControl->setValue($value[1]);
+
+    return $this;
+  }
+
+  public function getValue(): array {
+    return [
+      $this->startControl->getValue(),
+      $this->endControl->getValue(),
+    ];
+  }
+
+  public function isValid(): bool {
+    return $this->startControl->isValid() && $this->endControl->isValid();
+  }
+
+  public function isRequired(): bool {
+    return $this->startControl->isRequired() || $this->endControl->isRequired();
+  }
+
+  public function isEmpty(): bool {
+    throw !$this->startControl->isEmpty() || !$this->endControl->isEmpty();
+  }
+
+  public function getSerializedValue(): string {
+    return phutil_json_encode($this->getValue());
+  }
+
+  public function readSerializedValue($value) {
+    $this->setValue(phutil_json_decode($value));
+    return $this;
+  }
+
+  public function readValueFromRequest(AphrontRequest $request): array {
+    $this->startControl->readValueFromRequest($request);
+    $this->endControl->readValueFromRequest($request);
+
+    return $this->getValue();
+  }
+
+  protected function renderInput(): PhutilSafeHTML {
+    return javelin_tag(
+      'div',
+      [
+        'class' => 'aphront-form-date-input-container',
+      ],
+      [
+        $this->startControl->renderInput(),
+        $this->endControl->renderInput(),
+      ]);
+  }
+
+  protected function getCustomControlClass(): string {
+    return 'aphront-form-control-daterange';
+  }
+
+}
