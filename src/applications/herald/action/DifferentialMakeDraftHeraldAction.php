6bacfa08d2deca9c7e189eef9647903645c46269
Andrew He <ahe@freelancer.com>
2021-10-29 15:59:18 +1100

    draft only on creation

--- src/applications/herald/action/DifferentialMakeDraftHeraldAction.php
+++ src/applications/herald/action/DifferentialMakeDraftHeraldAction.php
@@ -66,31 +66,6 @@ final class DifferentialMakeDraftHeraldAction
   public function isValidEffect($revision) {
     $adapter = $this->getAdapter();
     $viewer = $adapter->getViewer();
-    $xactions = $adapter->getAppliedTransactions();
-
-    if (count($xactions) > 1) {
-      // if it entered this block, it means that there are more than one transaction
-      // done to the object with one click/command
-      // i.e.
-      // comment + request review
-      // comment + request review + change reviewers
-      $request_actions = array_filter($xactions, function ($xaction) {
-          return ($xaction->getTransactionType() == DifferentialRevisionRequestReviewTransaction::TRANSACTIONTYPE);
-      });
-
-      if ($request_actions) {
-          return false;
-      }
-
-      // or maybe we're updating a diff which is update + comment
-      $update_actions = array_filter($xactions, function ($xaction) {
-          return ($xaction->getTransactionType() == DifferentialRevisionUpdateTransaction::TRANSACTIONTYPE);
-      });
-
-      if ($update_actions) {
-        return true;
-      }
-    }
 
     $xactions = id(new DifferentialTransactionQuery())
         ->setViewer($viewer)
@@ -123,18 +98,6 @@ final class DifferentialMakeDraftHeraldAction
       if ($review_request_transaction_count === 1) {
           return !$request_review_from_draft_state;
       }
-    }
-
-    foreach ($xactions as $xaction) {
-        // we need to check if there's a transaction like `request-review`
-        // that caused the diff to be in `needs-review` status.
-        if ($xaction->getTransactionType() == DifferentialRevisionRequestReviewTransaction::TRANSACTIONTYPE) {
-            return false;
-        }
-        // however, if there's a more recent update, allow `draft` to take effect
-        if ($xaction->getTransactionType() == DifferentialRevisionUpdateTransaction::TRANSACTIONTYPE) {
-            return true;
-        }
     }
 
     return false;
