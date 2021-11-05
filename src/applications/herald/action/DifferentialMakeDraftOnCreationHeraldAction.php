f120dcc220a2f8795be815cd3823ca6015adc756
Andrew He <ahe@freelancer.com>
2021-11-05 14:18:21 +1100

    [phlab] Only draft differential revisions on creation
    Summary:
    ref T251838
    
    Weird/buggy behaviour:
    The logic that I changed for the existing herald action wouldnt reflect/apply in stg phab
    I ended up having to create a new herald rule in order for my changes to the logic to take effect
    {F2505001}
    ```name='logic of herald actions are the same', lines=20
    diff DifferentialMakeDraftHeraldAction.php TestHeraldAction.php
    3c3
    < final class DifferentialMakeDraftHeraldAction
    ---
    > final class DifferentialTestMakeDraftHeraldAction
    6,7c6,7
    <   const ACTIONCONST = 'differential.revision.make-draft';
    <   const DO_MAKE_DRAFT = 'revision.make-draft';
    ---
    >   const ACTIONCONST = 'differential.revision.test-make-draft';
    >   const DO_MAKE_DRAFT = 'revision.test-make-draft';
    10c10
    <     return pht('Make revision draft');
    ---
    >     return pht('test Make revision draft');
    15c15
    <       'Put revision in draft state.');
    ---
    >       'test Put revision in draft state.');
    23c23
    <         'name' => pht('Make draft'),
    ---
    >         'name' => pht('test Make draft'),
    32c32
    <           'Put revision in draft state.');
    ---
    >           'test Put revision in draft state.');
    ```
    
    Because of the above, I will add a new one in.
    The old herald action will not be removed as this will cause phabricator to break if the pages have any references to it.
    
    Test Plan:
    Manual tests:
    [x] Creating new diff with `arc diff` sets the differential revision to draft state
    [x] Creating new diff with `arc diff --only` sets it to draft state as well
    [x] Creating new diff with `arc diff --draft` works same way
    [x] publishing for review sends emails
    
    Test diffs in stg:
    https://phabricator.tools-stg.flnltd.com/D58
    https://phabricator.tools-stg.flnltd.com/D59
    https://phabricator.tools-stg.flnltd.com/D60
    
    This diff was made with --nounit since unit tests can only be run inside of the container
    Unit tests:
    ```
    root@ff02e67a5471:/usr/local/src/phlab# ../arcanist/bin/arc unit src/applications/herald/action/__tests__/*
       PASS  143ms   DifferentialMakeDraftHeraldActionTestCase::testIsValidEffect
       PASS   39ms★  DifferentialMakeDraftOnCreationHeraldActionTestCase::testIsValidEffect
    
    ```
    
    Reviewers: O8 Phabricator, #developer_experience, probles, hzhang
    
    Reviewed By: O8 Phabricator, #developer_experience, probles, hzhang
    
    Subscribers: hzhang
    
    Maniphest Tasks: T251838
    
    Differential Revision: https://phabricator.tools.flnltd.com/D174372

---
+++ src/applications/herald/action/DifferentialMakeDraftOnCreationHeraldAction.php
@@ -0,0 +1,105 @@
+<?php
+
+final class DifferentialMakeDraftOnCreationHeraldAction
+  extends HeraldAction {
+
+  const ACTIONCONST = 'differential.revision.make-draft-on-creation';
+  const DO_MAKE_DRAFT = 'revision.make-draft-on-creation';
+
+  public function getHeraldActionName() {
+    return pht('Make revision draft on creation');
+  }
+
+  public function renderActionDescription($value) {
+    return pht(
+      'Put revision in draft state on creation.');
+  }
+
+  protected function getActionEffectMap() {
+    return array(
+      self::DO_MAKE_DRAFT => array(
+        'icon' => 'fa-headphones',
+        'color' => 'red',
+        'name' => pht('Make draft on creation'),
+      ),
+    );
+  }
+
+  protected function renderActionEffectDescription($type, $data) {
+    switch ($type) {
+      case self::DO_MAKE_DRAFT:
+        return pht(
+          'Put revision in draft state on creation.');
+    }
+  }
+
+
+  public function supportsObject($object) {
+    return ($object instanceof DifferentialRevision);
+  }
+
+  public function supportsRuleType($rule_type) {
+    return $rule_type == HeraldRuleTypeConfig::RULE_TYPE_GLOBAL;
+  }
+
+  public function getHeraldActionStandardType() {
+    return self::STANDARD_NONE;
+  }
+
+  public function applyEffect($object, HeraldEffect $effect) {
+    if (
+      $object->isDraft() ||
+      !($object->isNeedsReview() || $object->isChangePlanned()) ||
+      !$this->isValidEffect($object)
+    ) {
+        return;
+    }
+
+    $object
+      ->setModernRevisionStatus(DifferentialRevisionStatus::DRAFT)
+      ->setShouldBroadcast(false)
+      ->setHoldAsDraft(true)
+      ->save();
+  }
+
+
+  public function isValidEffect($revision) {
+    $adapter = $this->getAdapter();
+    $viewer = $adapter->getViewer();
+
+    $xactions = id(new DifferentialTransactionQuery())
+        ->setViewer($viewer)
+        ->withObjectPHIDs(array($revision->getPHID()))
+        ->setOrder('newest')
+        ->setLimit(10)
+        ->execute();
+
+    // the logic below is for new diffs and we should ignore it if
+    // the transaction query returned 20 transactions which is our set limit
+    if (count($xactions) < 10) {
+      $request_actions = array_filter($xactions, function ($xaction) {
+          return ($xaction->getTransactionType() == DifferentialRevisionRequestReviewTransaction::TRANSACTIONTYPE);
+      });
+
+      $draft_actions = array_filter($xactions, function ($xaction) {
+          return ($xaction->getTransactionType() == DifferentialRevisionHoldDraftTransaction::TRANSACTIONTYPE);
+      });
+
+      $review_request_transaction_count = count($request_actions);
+      $draft_transaction_count = count($draft_actions);
+
+      $request_review_from_draft_state = $draft_transaction_count === 1;
+
+      // revisions created from `--draft` flag have no review request transaction
+      // meanwhile, revisions created with `--only` have one review request transaction
+      // i.e.
+      // it's not valid if it's a draft that's being published
+      // it's valid if it's a revision being created for the first time
+      if ($review_request_transaction_count === 1) {
+          return !$request_review_from_draft_state;
+      }
+    }
+
+    return false;
+  }
+}
